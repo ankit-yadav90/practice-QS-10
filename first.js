27b71a6a02e270126aee0f2df2849d6b0b658101
ankit-yadav90 <ankityadav73699@gmail.com>
2025-06-08 08:43:02 +0530

    new class include

--- first.js
+++ first.js
@@ -11,7 +11,18 @@ class User {
     }
 }
 
+class Admin extends User {
+     constructor(name, email) {
+            super(name, email);
+     }
+    editData() {
+        DATA = "some new valu";
+    }
+}
+
 let student1 = new User("ankit", "abc@email.com");
 let student2 = new User("aman", "aman@email.com");
 
 let teacher1 = new User("Dean", "Dean@email.com");
+
+let admin1 = new Admin("admin", "admin@email.com");
